b4771c3c50bc6e2ba7c920c947f427d6d3f491eb
younboyoung <boyung1021@naver.com>
2021-12-23 23:49:20 +0900

    2021.12.23

--- 04-04모든 아나그램 찾기/모든 아나그램 찾기.java
+++ 04-04모든 아나그램 찾기/모든 아나그램 찾기.java
@@ -1,24 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
-        int S = in.nextInt();
-        int T = in.nextInt();
+        String S = in.next();
+        String T = in.next();
         int cnt = 0;
 
-        HashMap<Integer, Integer> am = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         for(char x : T.toCharArray()){
             map.put(x, map.getOrDefault(x, 0) + 1);
         }
 
         int lt = 0, rt = T.length() - 1;
-
+        String answer;
         for(int i=0; i < S.length() - T.length() + 1; i++){
+            answer = "YES";
             String sub = S.substring(i, i + T.length());
             for(char x : sub.toCharArray()){
-                if(map.get(x) == 0 || !map.contains(x)) break;
+                if(map.get(x) == 0 || !map.containsKey(x)) answer = "NO";
             }
+            if(answer == "YES") cnt++;
         }
 
         System.out.println(cnt);
